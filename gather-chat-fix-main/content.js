0f0e5c840aaff91ee483df985c95366347687b1e
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-03-26 02:49:34 +0900

    fix: 선택자 선택 문제 추가 수정

--- gather-chat-fix-main/content.js
+++ gather-chat-fix-main/content.js
@@ -1,16 +1,14 @@
 var $게더채팅창;
 var 타이머_검색해조세요 = setInterval(() => {
-  const $채팅_아바타_아이콘을_먼저_찾습니다 =
-    document.querySelector('img[size="24"]');
+  const $채팅_아바타_아이콘을_먼저_찾습니다 = document.querySelector('img[size="24"]');
   if (!$채팅_아바타_아이콘을_먼저_찾습니다) return;
 
-  $게더채팅창 =
-    $채팅_아바타_아이콘을_먼저_찾습니다.closest('div[class*="css"]');
+  $게더채팅창 = $채팅_아바타_아이콘을_먼저_찾습니다.closest(".Layout").closest('div[class*="css-"]');
   $게더채팅창.addEventListener("DOMNodeInserted", 스크롤씨움직여주세요);
 
   clearInterval(타이머_검색해조세요);
 }, 5000);
 
 스크롤씨움직여주세요 = () => {
-  $게더채팅창.scrollTo({ top: $container.scrollHeight, behavior: "smooth" });
+  $게더채팅창.scrollTo({ top: $게더채팅창.scrollHeight, behavior: "smooth" });
 };
